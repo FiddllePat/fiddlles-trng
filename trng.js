b347b907f41e1360b01af382ee4094d9e48e0e26
FiddllePat <89714380+FiddllePat@users.noreply.github.com>
2023-08-01 16:47:59 +1000

    Update trng.js

--- trng.js
+++ trng.js
@@ -1,26 +1,69 @@
 const express = require('express');
-const app = express();
 const EventSource = require('eventsource');
-app.get('/trng', (req, res) => {
-    res.setHeader('Content-Type', 'text/event-stream');
-    res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Connection', 'keep-alive');
 
-    setInterval(() => {
-        const nanotime = process.hrtime.bigint();
-        res.write(`data: ${nanotime.toString()}\n\n`);
-    }, 1);
-});
-async function trng() {
-    await new Promise((resolve) => {
-        app.listen(58767, resolve);
+let servers;
+
+async function startServers(ports) {
+    servers = await Promise.all(ports.map(port => startServer(port)));
+}
+
+async function stopServers() {
+    await Promise.all(servers.map(server => stopServer(server)));
+}
+
+async function startServer(port) {
+    const app = express();
+
+    app.get('/trng', (req, res) => {
+        res.setHeader('Content-Type', 'text/event-stream');
+        res.setHeader('Cache-Control', 'no-cache');
+        res.setHeader('Connection', 'keep-alive');
+
+        setInterval(() => {
+            const nanotime = process.hrtime.bigint();
+            res.write(`data: ${nanotime.toString()}\n\n`);
+        }, 2);
     });
-    let result = await measure();
+
+    const server = app.listen(port);
+    await new Promise((resolve) => server.on('listening', resolve));
+    server.port = port;
+    return server;
+}
+
+async function stopServer(server) {
+    return new Promise((resolve) => {
+        server.close(resolve);
+    });
+}
+
+async function generate(port) {
+    let result = await measure(port);
     return Number(result);
 }
-function measure() {
+
+async function generate_amount(x, range, numDigits = 1, ports = [58767]) {
+    await startServers(ports);
+    const numbers = [];
+    
+    for (let i = 0; i < x; i++) {
+        let digitStrings = [];
+        for (let j = 0; j < numDigits; j++) {
+            let num = await generate(ports[j % ports.length]);
+            digitStrings.push(num);
+        }
+        
+        let num = combineDigitsIntoNumber(digitStrings, range[0], range[1]);
+        numbers.push(num);
+    }
+
+    await stopServers();
+    return numbers;
+}
+
+function measure(port) {
     return new Promise((resolve, reject) => {
-        var eventSource = new EventSource('http://localhost:58767/trng');
+        var eventSource = new EventSource(`http://localhost:${port}/trng`);
         let lastLatency = null;
         eventSource.onmessage = function(event) {
             const receivedTime = BigInt(event.data);
@@ -40,4 +83,23 @@ function measure() {
         };
     });
 }
-module.exports = trng;
+
+function combineDigitsIntoNumber(digitStrings, min, max) {
+    let maxes = [];
+    for (let i = 0; i < digitStrings.length; i++) {
+        maxes.push(Math.pow(10, i) * 9);
+    }
+    let num = 0;
+    for (let i = 0; i < digitStrings.length; i++) {
+        num += Number(digitStrings[i]) / 9 * maxes[i];
+    }
+    num = min + num / maxes.reduce((a, b) => a + b, 0) * (max - min);
+    num = Math.round(num);
+
+    return num;
+}
+
+module.exports = {
+    generate,
+    generate_amount
+};
